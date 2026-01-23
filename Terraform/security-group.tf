95ff4dfdc593e60157c66522160f9afaf3258926
jp <jribeiro.devops@gmail.com>
2026-01-23 19:15:38 -0300

    Edição em  arquivos

--- Terraform/security-group.tf
+++ Terraform/security-group.tf
@@ -2,31 +2,28 @@ resource "aws_security_group" "web_sg" {
   name        = "projeto-cicd-sg"
   description = "Security group para projeto CI/CD"
 
-  # Permite SSH (porta 22)
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # De qualquer IP
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Permite HTTP (porta 80)
   ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # De qualquer IP
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Permite toda saída
   egress {
     description = "Allow all outbound"
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"          # Todos os protocolos
-    cidr_blocks = ["0.0.0.0/0"] # Para qualquer destino
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
